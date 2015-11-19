96ee2a83806601e6f35f453a51f8cb21470c4374
Shelley Doljack <shelleydoljack@comcast.net>
2015-11-19 13:18:14 -0800

    Add more spec tests that will fail

--- spec/works/fake_work_spec.rb
+++ spec/works/fake_work_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 require 'linkeddata'
 
 describe 'fake test' do
-	
+	it 'fake spec' do
+		fail 'this is gonna fail'
+	end
+
+	it 'another fake spec' do
+		fail 'this is also gonna fail'
+	end
 end
